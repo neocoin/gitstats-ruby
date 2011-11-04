a1704c5832782c904c79774edeb7ab41d2992909
Christoph Plank <chrisistuff@gmail.com>
2011-11-04 22:45:36 +0100

    added optparser to main program

--- gitstats.rb
+++ gitstats.rb
@@ -5,6 +5,7 @@ require 'haml'
 require 'sass'
 require 'compass'
 require 'ftools'
+require 'optparse'
 
 $: << File.dirname($0)
 
@@ -13,25 +14,54 @@ require 'lib/stats'
 require 'lib/statgen'
 require 'lib/renderer'
 
-require 'pp'
+$options = {
+  :out => 'out',
+  :template => 'template',
+  :verbose => false,
+  :debug => false
+}
 
-#git = Git.new('../gitolite')
-#pp git.get_commits
+parser = OptionParser.new do |opts|
+  opts.banner = 'Usage: gitstats.rb [options] <gitdir1:[ref1]> [<gitdir2:[ref2]> ...]'
 
-stat = StatGen.new
-stat << '../gitolite'
+  opts.on('-o', '--out=arg', 'output directory') do |arg|
+    $options[:out] = arg
+  end
+
+  opts.on('-t', '--template=arg', 'template directory') do |arg|
+    $options[:template] = arg
+  end
+
+  opts.on('-v', '--[no-]verbose', 'verbose mode') do |arg|
+    $options[:verbose] = arg
+  end
+
+  opts.on('-d', '--[no-]debug', 'print debug messages') do |arg|
+    $options[:debug] = arg
+  end
+
+  opts.on_tail('-h', '--help', 'this help') do
+    puts opts
+    exit 0
+  end
+end
+
+parser.parse!
+
+if ARGV.empty?
+  puts parser
+  exit 1
+end
+
+stat = StatGen.new($options[:debug])
+
+ARGV.each do |path|
+  path, ref = path.split(':')
+  ref ||= 'HEAD'
+  stat << [path, ref]
+end
 stat.calc
-#pp stat.num_authors
-#pp stat.num_commits
-#pp stat.author_stats
-#pp stat.yearmonth_stats.diag(:commits)
-#pp stat.commits_per_author
-#pp stat.commits_per_author_per_year
-#pp stat.commits_per_author_per_month_year
-#pp stat.commits_per_month
-#pp stat.commits_per_year
-#pp stat.commits_per_month_year
 
-renderer = Renderer.new('template', 'out')
+renderer = Renderer.new($options[:template], $options[:out])
 renderer.render(stat)
 
